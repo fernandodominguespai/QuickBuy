9c0149de76ff60f2d26c2efa6e62576f1f276407
Fernando Domingues <fernandodominguespai@hotmail.com>
2020-03-26 19:04:43 -0300

    criação das entidades

---
+++ QuickBuy.Domain/Entidades/Pedido.cs
@@ -0,0 +1,27 @@
+﻿using QuickBuy.Domain.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Domain.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento{ get; set; }
+
+        /// <summary>
+        /// pedido deve ter um item ou muitos itens
+        /// </summary>
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+
+    }
+}
---
+++ QuickBuy.Domain/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,27 @@
+﻿using QuickBuy.Domain.Enumerados;
+
+namespace QuickBuy.Domain.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Decricao { get; set; }
+        public bool EhBoleto 
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool EhTransferencia
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Transferencia; }
+        }
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
