3072c7d5d5ec24d44e3eddee84a9806edfa6b9c6
Fernando Domingues <fernandodominguespai@hotmail.com>
2020-04-20 16:14:46 -0300

    aula 104 criados serviço e controller usuario

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
 import { Router, ActivatedRoute } from "@angular/router";
+import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
 
 @Component({
   selector: "app-login",
@@ -12,24 +13,33 @@ export class LoginComponent implements OnInit {
   public usuario;
   public returnUrl: string;
 
-  public usuarioAutenticado: boolean;
-
+  //public usuarioAutenticado: boolean;
   //public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"];
 
-  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+  constructor(private router: Router, private activatedRouter: ActivatedRoute,
+    private usuarioServico: UsuarioServico) {
   }
-    ngOnInit(): void {
-      this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
-      this.usuario = new Usuario();
-    }
+  ngOnInit(): void {
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
+    this.usuario = new Usuario();
+  }
 
   entrar() {
-    if (this.usuario.email == 'fer@eu' && this.usuario.senha == '123') {
-      this.usuarioAutenticado = true;
-      sessionStorage.setItem("usuario-Autenticado", "1");
-      this.router.navigate([this.returnUrl]);
-    }
-    alert(this.usuario.email + ' ' + this.usuario.senha);
+
+    this.usuarioServico.verificarUsuario(this.usuario)
+      .subscribe(
+        data => {
+        },
+        err => {
+        }
+      );
+
+    //if (this.usuario.email == 'fer@eu' && this.usuario.senha == '123') {
+    //  this.usuarioAutenticado = true;
+    //  sessionStorage.setItem("usuario-Autenticado", "1");
+    //  this.router.navigate([this.returnUrl]);
+    //}
+    //alert(this.usuario.email + ' ' + this.usuario.senha);
   }
   //public email = "";
   //public senha = "";
---
+++ QuickBuy.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickBuy.Web.Controllers
+{
+    public class UsuarioController: Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+
+            }
+        }
+
+        [HttpPost("VerificarUsuario")]
+        public ActionResult VerificarUsuario([FromBody]Usuario usuario)
+        {
+            try
+            {
+                if (usuario.Email == "fer@eu" && usuario.Senha == "123")
+                    return Ok(usuario);
+                else
+                    return BadRequest("Usuário ou senha inválido");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+                
+            }
+        }
+    }
+}
